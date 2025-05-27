e7708ebf5d5e5acb1a651fbfd050db31544bf1e2
OlamideCloudGRC <olamide.cloudgrc@gmail.com>
2025-05-27 14:28:17 -0400

    Add outputs for log bucket name and ARN

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,3 +7,13 @@ output "trigger_bucket_name" {
   description = "The name of the trigger bucket"
   value = aws_s3_bucket.trigger_bucket.id
 }
+
+output "log_bucket_name" {
+ description = "The name of the trigger bucket"   
+ value = aws_s3_bucket.log_bucket.id
+}
+
+output "log_bucket_arn" {
+  description = "The arn of the log bucket"
+  value = aws_s3_bucket.log_bucket.arn
+}
