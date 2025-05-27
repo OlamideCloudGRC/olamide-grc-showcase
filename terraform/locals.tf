ec0c4ab38c4587019c2508d8eb58c5f9a4497ddc
OlamideCloudGRC <olamide.cloudgrc@gmail.com>
2025-05-27 09:39:58 -0400

    Add reusable standard_tags for consistent GRC-compliant tagging

---
+++ terraform/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  standard_tags = merge(
+    {
+        Terraform = "true"
+        LastModified = formatdate("YYYY-MM-DD", timestamp())
+        Environment = var.environment
+    },
+    var.compliance_tags
+  )
+}
