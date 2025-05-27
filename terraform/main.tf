d83914ac136c32fc066a5b9facf56f8e672985c9
OlamideCloudGRC <olamide.cloudgrc@gmail.com>
2025-05-27 14:10:24 -0400

    Enable server-side encryption on log bucket using KMS and bucket keys

--- terraform/main.tf
+++ terraform/main.tf
@@ -241,3 +241,14 @@ resource "aws_kms_key_policy" "log_key_policy" {
   })
 }
 
+# Enable server side encryption with KMS key
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.log_encryption.arn
+      sse_algorithm     = "aws:kms"
+    }
+    bucket_key_enabled = var.enable_bucket_key
+  }
+}
