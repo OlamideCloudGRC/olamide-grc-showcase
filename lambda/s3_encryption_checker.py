b15640ff8eb8133b0fb23aa7eccd90bda32763ca
OlamideCloudGRC <olamide.cloudgrc@gmail.com>
2025-05-31 17:11:32 -0400

    Add severity levels, custom exception class, and S3 client setup

--- lambda/s3_encryption_checker.py
+++ lambda/s3_encryption_checker.py
@@ -8,9 +8,16 @@ Features:
     - Risk-prioritized logging
 
 """
+#==========================================#
+#                Module Imports            #
+#==========================================#
+import boto3
+from enum import IntEnum
 
 
-#----------Constants----------#
+#==========================================#
+#                Constants                 #
+#==========================================#
 COMPLIANCE_STANDARDS = [
     # Encryption of stored PANS
     "PCI-DSS 4.0 Requirement 3.3.1",
@@ -21,6 +28,33 @@ COMPLIANCE_STANDARDS = [
     # Encryption of ePHI
     "HIPAA 164.312(a)(2)(iv)"
 ]
-
 # Listing the required encryptions
 REQUIRED_ENCRYPTION = ["aws:kms"]
+
+
+#==========================================#
+#             Service Clients              #
+#==========================================#
+s3 = boto3.client('s3')
+
+#==========================================#
+#       Class Definitions                  #
+#==========================================#
+
+# Define the severity levels Enum class
+class SeverityLevel(IntEnum):
+   CRITICAL = 4  # Compliance Violations
+   HIGH = 3      # Security misconfiguration
+   MEDIUM = 2    # Operational warnings
+   LOW = 1       # Informational
+
+# Define Encryption violations Exception class
+class EncryptionViolations(Exception):
+   def __init__(self, bucket: str, key:str, found_encryption: str):
+      self.bucket = bucket
+      self.key = key
+      self.found_encryption = found_encryption
+      super().__init__(
+         f"Non-compliant encryption in s3://{bucket}/{key}. "
+         f"Found: {found_encryption}, Required: {REQUIRED_ENCRYPTION}"
+      )
